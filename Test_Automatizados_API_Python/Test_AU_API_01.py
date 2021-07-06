72d95a55a461fab2db48f0b6268b2d5ce9df8474
Cristian Parada <cmpmendoza12@gmail.com>
2021-07-06 17:52:48 -0600

    "Creacion de carpeta de test de pruebas automatizadas de API's con Python y primer test de pruebas"

---
+++ Test_Automatizados_API_Python/Test_AU_API_01.py
@@ -0,0 +1,19 @@
+import unittest
+
+class Test_AU_API_01(unittest.TestCase):
+
+      def setUp(self):
+          pass
+
+
+      def testName(self):
+          pass
+
+      def testName2(self):
+          pass
+
+      def tearDown(self):
+          pass
+
+      if __name__== "__main__":
+          unittest.main()
